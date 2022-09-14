2e30793ac813920171b261e02cfbeb01db13992a
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2022-09-14 15:44:28 +0200

    added scroll to top btn
    and made links made to different sections

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 console.log("script is running....")
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+
+
+    if (document.documentElement.scrollTop > 300) {
+        document.getElementById("myBtn").style.opacity = "100";
+    } else {
+        document.getElementById("myBtn").style.opacity = "0";
+    }
+
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}

ab9ee3c4e04390342cda643971dddf830dcd66ac
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2022-09-15 14:38:25 +0200

    all the layout is done
    might be minor thinks that has to be clean up abit

--- javascript.js
+++ javascript.js
@@ -16,4 +16,36 @@ function scrollFunction() {
 
 function topFunction() {
     document.documentElement.scrollTop = 0;
+};
+
+function validateForm() {
+
+    let x = document.forms["news_letter"]["fname"].value;
+    let y = document.forms["news_letter"]["phone"].value.length;
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let z = document.forms["news_letter"]["email"].value.match(validRegex);
+
+
+    if (x == "") {
+        document.getElementById("fname").style.borderColor = "red";
+
+    } else {
+        document.getElementById("fname").style.borderColor = "transparent";
+    };
+
+    if (y != 8) {
+        document.getElementById("phone").style.borderColor = "red";
+
+    } else {
+        document.getElementById("phone").style.borderColor = "transparent";
+    }
+
+    if (z == null) {
+        document.getElementById("email").style.borderColor = "red";
+    } else {
+        document.getElementById("email").style.borderColor = "transparent";
+    }
+
+
+
 }
